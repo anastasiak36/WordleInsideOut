c09f824ca2611f8b61d423b3ccd83bd503e15327
anastasiak36 <akelnik.9@gmail.com>
2022-06-05 19:11:04 -0400

    coded input for tiles

--- wordlejs.js
+++ wordlejs.js
@@ -1,8 +1,10 @@
-var button = document.getElementById("button").onclick = getLevel;
-var radios = document.getElementsByName('level');
+var currentRow = 0;
+var currentCol = 0;
+var radios = document.getElementsByName("level");
+var button = document.getElementById("button");
+button.onclick = getLevel;
 
 function getLevel() {
-
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
             // do whatever you want with the checked radio
@@ -26,7 +28,27 @@ function createGrid() {
             var gridTile = document.createElement("span");
             gridTile.id = row.toString() + col.toString();
             gridTile.className = "gridTile";
+            gridTile.innerText = "";
             document.getElementById("grid").appendChild(gridTile);
         }
     }
 }
+document.addEventListener("keyup", (e) => {
+    if ("KeyA" <= e.code && e.code <= "KeyZ") {
+        if (currentCol < 5) {
+            var currentGridTile = document.getElementById(currentRow.toString() + currentCol.toString());
+            if (currentGridTile.innerText == "") {
+                currentGridTile.innerText = e.code[3];
+                currentCol += 1;
+            }
+        }
+    }
+
+    else if (e.code == "Backspace"){
+        if (0 < currentCol && currentCol <= 5) {
+            currentCol -= 1;
+        }
+        var currentGridTile = document.getElementById(currentRow.toString() + currentCol.toString());
+        currentGridTile.innerText = "";
+    }
+})
